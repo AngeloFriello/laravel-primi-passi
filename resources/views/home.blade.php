8bdc2eeac4443018587df2b448c31fb28a34e2f8
angelo friello <angelofriello01@gmail.com>
2023-12-12 09:43:08 +0100

    add project primi-passi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <ul>
+        <li><a href="{{route('chi siamo')}}">chi siamo</a></li>
+        <li><a href="">cosa facciamo</a></li>
+        <li><a href="">da dove veniamo</a></li>
+        <li><a href="">perchè veniamo</a></li>
+    </ul>
+
+</body>
+</html>
